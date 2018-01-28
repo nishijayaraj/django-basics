1e7436d7484ffcb09c10efd9c1949f1399f2bab2
nishijayaraj <nishi@debian>
2018-01-28 15:28:59 +0530

    routing added to the app music,  /music

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.shortcuts import render
-
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("<h1> this is the music app home page </h1>")	
+
